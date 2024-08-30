8ef50239be1b6f63c652a9c9603d08849c4671b7
ismylsmylv <ismayilovsmyl2020@gmail.com>
2024-08-30 18:27:26 +0400

    feat: task listing added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,19 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { TodoItem } from '../shared/modules/todoItem';
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
+  items = [
+    new TodoItem('To learn Angular'),
+    new TodoItem('Have a coffee', true),
+    new TodoItem('Browse Linkedin'),
+  ];
   title = 'angular-todo';
 }
