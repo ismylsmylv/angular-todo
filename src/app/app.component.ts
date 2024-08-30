a48e0f9f93b764227d334f2c8087b9544ca39345
ismylsmylv <ismayilovsmyl2020@gmail.com>
2024-08-30 20:03:43 +0400

    feat: add task added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,18 +2,27 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { TodoItem } from '../shared/modules/todoItem';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CommonModule],
+  imports: [RouterOutlet, CommonModule, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
   items: TodoItem[] = [
-    new TodoItem('To learn Angular'),
-    new TodoItem('Have a coffee', true),
-    new TodoItem('Browse Linkedin'),
+    // new TodoItem('To learn Angular'),
+    // new TodoItem('Have a coffee', true),
+    // new TodoItem('Browse Linkedin'),
   ];
   title = 'angular-todo';
+  toggleItem(item: TodoItem) {
+    item.isComplete = !item.isComplete;
+  }
+  addTask() {
+    this.items.push(new TodoItem(this.newTaskText));
+    this.newTaskText = '';
+  }
+  newTaskText: string = '';
 }
